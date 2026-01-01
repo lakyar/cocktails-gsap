988efb573a2440b239faece88ff80c36d37378a4
Lak Yar <lynn39408@gmail.com>
2026-01-01 18:45:32 +0630

    lenis smooth scroll effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { gsap } from "gsap"
 import {ScrollTrigger,SplitText} from "gsap/all"
+import Lenis from 'lenis';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
@@ -10,6 +12,20 @@ import Menu from './components/Menu';
 import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const lenis = new Lenis({
+  duration: 1.2,
+  smooth: true,
+  smoothTouch: false
+});
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
 
 const App = () => {
   return (
